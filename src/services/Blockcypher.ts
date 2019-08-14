b59efd410a4094e1d6f1bccce3a67e748e2cc0f5
zeluisping <zeluis.100@gmail.com>
2019-08-15 00:04:34 +0100

    probably final Blockcypher

--- src/services/Blockcypher.ts
+++ src/services/Blockcypher.ts
@@ -1,11 +1,40 @@
 import Axios from "axios";
 
+/*
+{
+  "address": "738d145faabb1e00cf5a017588a9c0f998318012",
+  "total_received": 9762206505909057760,
+  "total_sent": 6916970312523512365,
+  "balance": 2845236193385545395,
+  "unconfirmed_balance": 0,
+  "final_balance": 2845236193385545395,
+  "n_tx": 702,
+  "unconfirmed_n_tx": 0,
+  "final_n_tx": 702
+}
+*/
+interface IEthereumBalance {
+  address: string;
+  total_received: number;
+  total_sent: number;
+  balance: number;
+  unconfirmed_balance: number;
+  final_balance: number;
+  n_tx: number;
+  unconfirmed_n_tx: number;
+  final_n_tx: number;
+}
+
 export default new (class {
-  async FetchEthereumBalance(address: string) {
-    const res = await Axios.get(
+  async FetchEthereumBalance(
+    address: string
+  ): Promise<IEthereumBalance | null> {
+    const res = await Axios.get<IEthereumBalance>(
       `https://api.blockcypher.com/v1/eth/main/addrs/${address}/balance`
     );
-    // @todo handle res.status
-    return res.status === 200;
+    if (res.status === 200) {
+      return res.data;
+    }
+    return null;
   }
 })();
