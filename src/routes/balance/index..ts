d7428eb2e3da0cc863957575c9922ad250ae336e
zeluisping <zeluis.100@gmail.com>
2019-08-15 11:28:20 +0100

    use Blockcypher

--- src/routes/balance/index..ts
+++ src/routes/balance/index..ts
@@ -1,10 +1,21 @@
 import KoaRouter from "koa-router";
+import Blockcypher from "../../services/Blockcypher";
 
 const route = new KoaRouter();
 export default route;
 
 route.get("/:address", async (ctx, next) => {
-  // const address = ctx.params["address"];
+  const address = ctx.params["address"];
+  // @todo validate address?
 
+  const balance = await Blockcypher.FetchEthereumBalance(address);
+  if (balance === null) {
+    // @todo maybe return same code as Blockcypher?
+    ctx.response.status = 404;
+    await next();
+    return;
+  }
+
+  ctx.response.body = balance;
   await next();
 });
