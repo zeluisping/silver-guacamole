6fd2ddc255188d68c889b512fa87d860a430152a
zeluisping <zeluis.100@gmail.com>
2019-08-15 11:28:30 +0100

    add routes to app

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import Koa from "koa";
 import KoaLogger from "koa-logger";
+import Routes from "./routes";
 
 const port = process.env["PORT"] || 3000;
 const app = new Koa();
 
 app.use(KoaLogger());
+app.use(Routes.routes()).use(Routes.allowedMethods());
 app.listen(port, () => {
   console.log(`Server listening on port ${port}...`);
 });
