1630ccdd076f8534eb165d67ff0d15572cd70e17
zeluisping <zeluis.100@gmail.com>
2019-08-15 12:59:50 +0100

    use address validation

--- src/routes/balance/index..ts
+++ src/routes/balance/index..ts
@@ -1,16 +1,20 @@
 import KoaRouter from "koa-router";
 import Blockcypher from "../../services/Blockcypher";
+import ValidateEthereumAddress from "../../utils/ValidateEthereumAddress";
 
 const route = new KoaRouter();
 export default route;
 
 route.get("/:address", async (ctx, next) => {
-  const address = ctx.params["address"];
-  // @todo validate address?
+  const address = ValidateEthereumAddress(ctx.params["address"]);
+  if (typeof address === "undefined") {
+    ctx.response.status = 400;
+    await next();
+    return;
+  }
 
   const balance = await Blockcypher.FetchEthereumBalance(address);
   if (balance === null) {
-    // @todo maybe return same code as Blockcypher?
     ctx.response.status = 404;
     await next();
     return;
